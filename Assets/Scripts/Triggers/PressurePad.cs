a0eda6958182c42e6340ab8a7258b1691d87c7c1
James LaFritz <ktmarine1999@gmail.com>
2021-06-09 23:25:35 -0400

    Pusing Objects.

---
+++ Assets/Scripts/Triggers/PressurePad.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class PressurePad : MonoBehaviour
+{
+    // detect an object on the pressure pad
+    // when close to center
+    // stop the object from moving
+    // trigger a Unity Event
+}
